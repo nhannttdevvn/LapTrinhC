c1fa1ab75c0bfea2481f5236d035bae6c06eea1a
nhannttdevvn <nhanntt.devvn@gmail.com>
2023-10-04 22:58:07 +0700

    sua lab 2.9

--- LapTrinhC++/lab2.9.cpp
+++ LapTrinhC++/lab2.9.cpp
@@ -1,16 +1,15 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 int main()
-{   
-    int soTuNhien; donVi,chuc,tram,thuong;
+{
+    int soTuNhien, donVi, chuc, tram, thuong;
     cout << "Nhap so tu nhien co 3 chu so: ";
     cin << soTuNhien;
-    thuong=soTuNhien/10;
-    chuc=thuong%10;
-    donVi=soTuNhien%100;
-    tram=soTuNhien/100;
+    thuong = soTuNhien / 10;
+    chuc = thuong % 10;
+    donVi = soTuNhien % 100;
+    tram = soTuNhien / 100;
     cout << "So dao nguoc: " << donVi << chuc << tram;
-    
-    
+
     return 0;
 }
